a911a1eda7af4587b8f59b3475c6eceaf2eb878f
Alexandre Esteves <a13xandr3ea@gmail.com>
2024-11-01 16:59:35 -0300

    inclusao rota search

--- index.js
+++ index.js
@@ -39,6 +39,20 @@ client.connect()
     }
    }
 
+   app.get('/search/:palavrachave', async (req, res) => {
+    try {
+      const palavraChave = req.params.palavrachave;
+      const resultado = await db.collection('Item').find({ detalhe: new RegExp(palavraChave, 'i') }).toArray();
+      if (resultado.length === 0) {
+        return res.status(404).json({ error: 'Item não encontrado' });
+      }
+      res.status(200).json(resultado);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: 'Erro no servidor' });
+    }
+  });
+  
    // Rota para criar um novo item
    app.post('/items', async (req, res) => {
     try {
